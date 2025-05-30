3d687ce5f86f15aa29731b2b10058aa9a012aaad
Aisha Attar <aishaat@student.chalmers.se>
2025-05-30 14:12:15 +0200

    #12 Add clearsearch to searchbar

--- application/src/app/searchinput.tsx
+++ application/src/app/searchinput.tsx
@@ -1,21 +1,39 @@
-import styles from "./searchinput.module.css";
-import { FiSearch, FiX } from 'react-icons/fi';
+"use client";
 
-// Functional component for the search input
+import styles from "./searchinput.module.css";
+import { FiSearch, FiX } from "react-icons/fi";
+import { useState, ChangeEvent } from "react";
+
 const SearchInput = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const clearSearch = () => {
+    setInputValue("");
+  };
+
   return (
     <div className={styles.search_wrapper}>
-      <FiSearch className={styles.icon_left} />  
-
+      <FiSearch className={styles.icon_left} />
       <input
         className={styles.search_input}
+        type="text"
         placeholder="Enter course name..."
+        value={inputValue}
+        onChange={handleChange}
       />
-
-      <FiX className={styles.icon_right} />
+      {inputValue && (
+        <FiX
+          className={styles.icon_right}
+          onClick={clearSearch}
+        />
+      )}
     </div>
   );
 };
 
-
 export default SearchInput;
+
