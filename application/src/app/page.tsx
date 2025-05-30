dc0214a0e012275782acc587d5083853d12a30e6
Razvan Albu <razvan-albu@hotmail.com>
2025-05-30 14:12:55 +0200

    #12 Add initial list component implementation

--- application/src/app/page.tsx
+++ application/src/app/page.tsx
@@ -1,9 +1,21 @@
-"use client"
+"use client";
 import styles from "@/app/page.module.css";
+import List, { ListItem } from "@/components/list/list";
 import SearchInput from "@/components/search/searchinput";
 import { ReactSVG } from "react-svg";
 
 export default function Home() {
+  const data: Array<ListItem> = [
+    {
+      title: "Test3",
+      description: "Description",
+    },
+    {
+      title: "Test2",
+      description: "Description",
+    },
+  ];
+
   return (
     <div className={styles.page}>
       <div className={styles.title_container}>
@@ -11,6 +23,7 @@ export default function Home() {
         <h1 className={styles.title}>Starling Vox</h1>
       </div>
       <SearchInput />
+      <List data={data} />
     </div>
   );
 }
