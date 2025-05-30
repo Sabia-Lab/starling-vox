b32beb32be9ca3ef14c6a055357d7baf66c35a28
Aisha Attar <aishaat@student.chalmers.se>
2025-05-30 11:29:26 +0200

    #12 Add Searchbar to homepage

--- application/src/app/page.tsx
+++ application/src/app/page.tsx
@@ -1,13 +1,14 @@
 import styles from "./page.module.css";
+import SearchInput from "./searchinput";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <div className={styles.title_container}>
         <img src="./logo.png" className={styles.logo}/>
-          
         <h1 className={styles.title}>Starling Vox</h1>
       </div>
+        <SearchInput />
     </div>
   );
 }
