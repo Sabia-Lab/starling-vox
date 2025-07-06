c20fbbb158b3d62be429172f76abeace3d009c32
Intisar Warfa <warfa@student.chalmers.se>
2025-07-06 17:14:42 +0400

    #8 Add close button to popup

--- application/src/components/open_ended/OpenEndedContainer.tsx
+++ application/src/components/open_ended/OpenEndedContainer.tsx
@@ -1,19 +1,23 @@
 import AnswerBox from './AnswerBox';
 import styles from './open_ended.module.css';
+import { ReactSVG } from "react-svg";
 
-// This component is for grouping the most and least liked answer boxes together
+// This component is for grouping the most and least liked answer boxes together.
 
-type course = {
+type props = {
     courseName: string;
+    onClose: () => void
 }
 
-export default function OpenEndedContainer({ courseName }: course) {
-    
-
+export default function OpenEndedContainer({ courseName,onClose }: props) {
     return (
         <div className={styles.mainContainer}>
             <AnswerBox mostOrLeast="most" courseName={courseName} />
             <AnswerBox mostOrLeast="least" courseName={courseName} />
+
+        <button className={styles.closeButton} onClick={onClose}>
+         <ReactSVG src="images/close_button.svg" />
+        </button>
         </div> 
     )
 }
